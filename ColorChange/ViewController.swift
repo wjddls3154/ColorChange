8608ee0605857404f5b3dc354b651635e53e4238
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-03-14 15:33:50 +0900

    Color change & Text Change  Switch by Case

--- ColorChange/ViewController.swift
+++ ColorChange/ViewController.swift
@@ -26,22 +26,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var labC: UILabel!
     
     @IBAction func BtnChange(_ sender: Any) {
-        if btnc == 0 {
+        switch (btnc){
+        case 0:
             view.backgroundColor = UIColor.blue
             btnc = btnc + 1
             labC.text = "블루"
-        }
-        else if btnc == 1 {
+        case 1:
             view.backgroundColor = UIColor.brown
             btnc = btnc + 1
             labC.text = "갈색"
-        }
         
-        else if btnc == 2 {
+           
+        default:
             view.backgroundColor = UIColor.cyan
             btnc = btnc - 2
             labC.text = "하늘"
-            
         }
         
         
